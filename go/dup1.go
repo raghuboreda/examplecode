13fedefb5c346c26506be31971363c4d516aea1c
Raghu Boreda <raghub@gmail.com>
2016-09-13 12:06:50 -0700

    dup1 program

---
+++ go/dup1.go
@@ -0,0 +1,40 @@
+// Dup1 prints the text of each line that appears more than once in the standard
+// input, preceded by its count.
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    counts := make(map[string]int)
+    files := os.Args[1:]
+    if len(files) == 0 {
+        countLines(os.Stdin, counts)
+    } else {
+        for _, arg := range files {
+            f, err := os.Open(arg)
+            if err != nil {
+                 fmt.Fprintf(os.Stderr, "dup1: %v\n", err)
+                 continue
+            }
+            countLines(f, counts)
+            f.Close()
+        }
+    }
+    // NOTE: ignoring potential errors from input.Err()
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\n", n, line)
+        }
+    }
+}
+
+func countLines(f *os.File, counts map[string]int) {
+    input := bufio.NewScanner(f)
+    for input.Scan() {
+        counts[input.Text()]++
+    }
+}
